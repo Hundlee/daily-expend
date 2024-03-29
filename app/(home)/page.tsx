d417e6ab1ca0f13934dc5b0e0f2defca7e41055f
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-03-28 22:55:07 -0300

    feat: add spent component to home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,11 @@
 import Header from "../_components/header";
+import Spent from "./_components/spent";
 
 export default function Home() {
     return (
         <div>
             <Header />
+            <Spent />
         </div>
     );
 }
