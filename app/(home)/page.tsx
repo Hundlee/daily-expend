dfaa4977a5d82b06c27fab4b91c12270ab22c2b4
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-03-29 17:44:46 -0300

    featheader removed on homepage because I added it to the main layout

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,8 @@
-import Header from "../_components/header";
 import Spent from "./_components/spent";
 
 export default function Home() {
     return (
         <div>
-            <Header />
             <Spent />
         </div>
     );
