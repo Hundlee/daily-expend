c5531636d35385a1e492d943e5e7a14c06dea0ad
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-03-30 16:52:22 -0300

    feat: add route to homepage in navigation

--- app/_components/navigation.tsx
+++ app/_components/navigation.tsx
@@ -1,23 +1,24 @@
-import { BarChart4Icon, MegaphoneIcon, Plus } from "lucide-react";
-import { Button } from "./ui/button";
+import { BarChart4Icon, HomeIcon, MegaphoneIcon, Plus } from "lucide-react";
 import Link from "next/link";
+import ButtomCreateSpent from "./bottomCreateSpent";
 
 const Navigation = () => {
     return (
         <div className="p-5 flex items-center justify-between border-t border-border    ">
-            <div className="flex flex-col items-center cursor-pointer">
-                <Link href="/analytics">
+            <div>
+                <Link href="/analytics" className="flex flex-col items-center">
                     <BarChart4Icon />
                     <p>Revisão</p>
                 </Link>
             </div>
-            <Button className="rounded-full p-2">
-                <Plus />
-            </Button>
-            <div className="flex flex-col items-center cursor-pointer">
-                <MegaphoneIcon />
-                <p>Alterações</p>
-            </div>
+            <ButtomCreateSpent />
+            <Link
+                href="/"
+                className="flex flex-col items-center cursor-pointer"
+            >
+                <HomeIcon />
+                <p>Gastos</p>
+            </Link>
         </div>
     );
 };
