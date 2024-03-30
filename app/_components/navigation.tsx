f9da7d641f9444b67c9a33507fee8603efbba186
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-03-30 14:45:37 -0300

    feat: add redirection to analytics page

--- app/_components/navigation.tsx
+++ app/_components/navigation.tsx
@@ -1,12 +1,15 @@
 import { BarChart4Icon, MegaphoneIcon, Plus } from "lucide-react";
 import { Button } from "./ui/button";
+import Link from "next/link";
 
 const Navigation = () => {
     return (
         <div className="p-5 flex items-center justify-between border-t border-border    ">
             <div className="flex flex-col items-center cursor-pointer">
-                <BarChart4Icon />
-                <p>Revisão</p>
+                <Link href="/analytics">
+                    <BarChart4Icon />
+                    <p>Revisão</p>
+                </Link>
             </div>
             <Button className="rounded-full p-2">
                 <Plus />
