cf1cc62061ba9e4442c1dcac70da4d0c0571f967
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-03-31 13:19:37 -0300

    feat: remove hook useState

--- app/_components/bottomCreateSpent.tsx
+++ app/_components/bottomCreateSpent.tsx
@@ -4,7 +4,6 @@ import { Plus } from "lucide-react";
 import { Button } from "./ui/button";
 import CreateSpent from "./_controllers/createSpent";
 import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
-import { useEffect, useState } from "react";
 
 const ButtomCreateSpent = () => {
     return (
