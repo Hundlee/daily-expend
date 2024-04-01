678a3c11074cbe0236f937fdb7cab633c0ed773a
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-04-01 18:57:28 -0300

    feat: add loginButton component in header

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,8 +1,5 @@
-"use client";
-
-import { LogInIcon } from "lucide-react";
-import { Button } from "./ui/button";
 import ThemeToggle from "./themeToggle";
+import LoginButton from "./loginBottom";
 
 interface HeaderProps {
     name: string;
@@ -13,9 +10,7 @@ const Header = ({ name }: HeaderProps) => {
         <header className="flex items-center justify-between p-5">
             <ThemeToggle />
             <h1 className="text-2xl font-bold">{name}</h1>
-            <Button className="p-0" variant="ghost">
-                <LogInIcon />
-            </Button>
+            <LoginButton />
         </header>
     );
 };
