238f7fa2eb2f7e8512d9f6aff2c133982904fb0d
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-04-01 18:57:04 -0300

    feat: add function to save spent in database

---
+++ app/_actions/save-spent.tsx
@@ -0,0 +1,33 @@
+"use server";
+
+import { db } from "@/app/_lib/prisma";
+import { revalidatePath } from "next/cache";
+
+interface SaveSpentParams {
+    price: number;
+    tagNames: string[];
+    userId: string;
+    date: Date;
+    name: string;
+}
+
+export const saveSpent = async (params: SaveSpentParams) => {
+    const { price, tagNames, userId, date, name } = params;
+
+    const tags = await db.tag.findMany({
+        where: { name: { in: tagNames } },
+    });
+
+    await db.spent.create({
+        data: {
+            price,
+            tags: { connect: tags.map((tag) => ({ id: tag.id })) },
+            userId,
+            date,
+            name,
+        },
+    });
+
+    revalidatePath("/");
+    revalidatePath("/project/[id]");
+};
