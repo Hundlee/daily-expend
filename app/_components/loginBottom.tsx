11acf7d8ac79b17241c8e7e286962d744f2cb482
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-04-01 18:57:57 -0300

    feat: created loginButton component to login and logout page

---
+++ app/_components/loginBottom.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { LogInIcon, LogOutIcon } from "lucide-react";
+import { Button } from "./ui/button";
+import { signIn, signOut, useSession } from "next-auth/react";
+
+const LoginButton = () => {
+    const { data: session } = useSession();
+
+    const handleLogin = async () => {
+        await signIn("google");
+    };
+
+    const handleLogout = async () => {
+        await signOut();
+    };
+
+    return (
+        <div>
+            {session ? (
+                <Button variant="ghost" className="p-0" onClick={handleLogout}>
+                    <LogOutIcon />
+                </Button>
+            ) : (
+                <Button variant="ghost" className="p-0" onClick={handleLogin}>
+                    <LogInIcon />
+                </Button>
+            )}
+        </div>
+    );
+};
+
+export default LoginButton;
