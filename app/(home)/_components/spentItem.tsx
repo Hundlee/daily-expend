82275bb6377f455b16f339c0543d2d70522b73c6
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-05-03 15:09:37 -0300

    feat: passing the necessary parameters

--- app/(home)/_components/spentItem.tsx
+++ app/(home)/_components/spentItem.tsx
@@ -3,6 +3,7 @@
 import { Card, CardContent } from "@/app/_components/ui/card";
 import { Spent } from "@prisma/client";
 import { format } from "date-fns";
+import { SpentDelete } from "./spentDeleteComponent";
 
 interface SpentItemProps {
     spent: Spent;
@@ -10,14 +11,15 @@ interface SpentItemProps {
 const SpentItem = ({ spent }: SpentItemProps) => {
     return (
         <Card className="min-w-full last:mb-24">
-            <CardContent className="py-0 flex px-0 ">
-                <div className="flex flex-col gap-2 py-5 flex-[3] pl-5">
+            <CardContent className="py-0 flex px-0 items-center justify-between">
+                <div className="flex flex-col gap-2 py-5 pl-3">
                     <h2 className="font-bold">{spent.name}</h2>
                     <p className="text-sm">
                         {format(spent.date, "'Hoje' H:mm aa")}
                     </p>
                 </div>
-                <div className="flex flex-col items-center justify-center  p-3">
+                <SpentDelete spentId={spent} />
+                <div className="flex flex-col items-center justify-center pr-3">
                     <p className="text-destructive font-semibold">
                         - R$ {parseFloat(spent.price.valueOf()).toFixed(2)}
                     </p>
