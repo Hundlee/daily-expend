71a7fc1d16e63a288f2c566e299c4b1e0adac991
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-06-21 21:35:44 -0300

    feat: add variable if the expense was on the same day, 'today' will appear, but if not, the date will appear

--- app/(home)/_components/spentItem.tsx
+++ app/(home)/_components/spentItem.tsx
@@ -5,19 +5,32 @@ import { Spent } from "@prisma/client";
 import { format } from "date-fns";
 import { SpentDelete } from "./spentDeleteComponent";
 import UpdateSpentComponent from "@/app/(home)/_components/updateSpentComponent";
+import { ptBR } from "date-fns/locale";
 
 interface SpentItemProps {
     spent: Spent;
 }
 const SpentItem = ({ spent }: SpentItemProps) => {
+    const currentDate = new Date();
+
+    currentDate.setDate(currentDate.getDate() - 1);
+
     return (
         <Card className="w-full last:mb-24 lg:h-[10rem] lg:flex lg:items-center lg:justify-center ">
             <CardContent className="py-0 flex px-0 items-center justify-between lg:w-full">
                 <div className="flex flex-col gap-2 py-5 pl-3 w-32 lg:gap-5">
                     <h2 className="font-bold">{spent.name}</h2>
-                    <p className="text-sm">
-                        {format(spent.date, "'Hoje' H:mm aa")}
-                    </p>
+                    {spent.date >= currentDate ? (
+                        <p className="text-sm">
+                            {format(spent.date, "'Hoje' H:mm aa")}
+                        </p>
+                    ) : (
+                        <p className="text-sm capitalize">
+                            {format(spent.date, "'dia' d 'de' MMM H:mm aa", {
+                                locale: ptBR,
+                            })}
+                        </p>
+                    )}
                 </div>
                 <div className="flex flex-col">
                     <UpdateSpentComponent spent={spent} />
