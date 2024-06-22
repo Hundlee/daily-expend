5ffba26f29ba608d0d71f451415385199574ed57
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-06-22 20:39:32 -0300

    refactor: refactored code using functions like fetchSpentData and filterSpent

--- app/analytics/_components/lastWeekSpent.tsx
+++ app/analytics/_components/lastWeekSpent.tsx
@@ -1,46 +1,29 @@
 import { authOptions } from "@/app/_lib/auth";
-import { db } from "@/app/_lib/prisma";
-import { endOfMonth, startOfMonth } from "date-fns";
 import { getServerSession } from "next-auth";
 import SpentItemAnalytics from "./spentItemAnalytics";
+import { fetchSpentData } from "@/app/_helpers/fetchSpentData";
+import { filterSpent } from "@/app/_helpers/filterSpent";
+import { Spent } from "@prisma/client";
 
 export default async function LastWeekSpent() {
     const session = await getServerSession(authOptions);
 
     const currentDate = new Date();
-    const firstDayOfMonth = startOfMonth(currentDate);
-    const lastDayOfMonth = endOfMonth(currentDate);
 
-    const spent = session?.user
-        ? await db.spent.findMany({
-              where: {
-                  userId: (session.user as any).id,
-                  date: {
-                      gte: firstDayOfMonth,
-                      lte: lastDayOfMonth,
-                  },
-              },
-          })
-        : [];
+    const spent = await fetchSpentData(session, currentDate);
 
-    const spentLastWeekFilter = spent.filter((spent) => {
-        const oneWeekAgo = new Date();
-        oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
-        return spent.date >= oneWeekAgo;
-    });
+    const spentLastWeek = filterSpent(spent, currentDate, "lastWeek");
 
-    spentLastWeekFilter.sort((a, b) => {
-        return Number(b.price) - Number(a.price);
-    });
+    spentLastWeek.sort((a: any, b: any) => Number(b.price) - Number(a.price));
 
-    const topThreeSpentLastWeek = spentLastWeekFilter.slice(0, 3);
+    const topThreeSpentLastWeek = spentLastWeek.slice(0, 3);
 
     return (
         <section className="p-5">
             <h1 className="text-white">Maiores gastos da ultima semana</h1>
 
             <div className="flex flex-col gap-3 pt-5">
-                {topThreeSpentLastWeek.map((spent) => (
+                {topThreeSpentLastWeek.map((spent: Spent) => (
                     <SpentItemAnalytics key={spent.id} spent={spent} />
                 ))}
             </div>
