ba7c72c6187755f30860d2a949c52846bdfb6dd3
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-06-22 20:33:25 -0300

    refactor: Refactor Home component
    - Replaced direct data fetching and processing with helper functions.
    - Utilized fetchSpentData for fetching spent data.
    - Implemented calculateTotals to compute total spendings.
    - Added filterSpent to filter spent data by 'today' and 'lastWeek'.
    - Enhanced code readability and maintainability.

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,60 +1,40 @@
 import { getServerSession } from "next-auth";
 import Header from "../_components/header";
 import { authOptions } from "../_lib/auth";
-import { db } from "../_lib/prisma";
 import SpentItem from "./_components/spentItem";
-import { endOfMonth, format, startOfMonth } from "date-fns";
+import { format } from "date-fns";
 import { ptBR } from "date-fns/locale";
+import { fetchSpentData } from "../_helpers/fetchSpentData";
+import { calculateTotals } from "../_helpers/calculateTotals";
+import { filterSpent } from "../_helpers/filterSpent";
+import { Spent } from "@prisma/client";
 
 export default async function Home() {
     const session = await getServerSession(authOptions);
-
     const currentDate = new Date();
-    const firstDayOfMonth = startOfMonth(currentDate);
-    const lastDayOfMonth = endOfMonth(currentDate);
 
-    const spent = session?.user
-        ? await db.spent.findMany({
-              where: {
-                  userId: (session.user as any).id,
-                  date: {
-                      gte: firstDayOfMonth,
-                      lte: lastDayOfMonth,
-                  },
-              },
-          })
-        : [];
+    const spent = await fetchSpentData(session, currentDate);
+    const { totalToday, totalWeek, totalMonth } = calculateTotals(
+        spent,
+        currentDate
+    );
 
-    const totalTodaySpending = spent.reduce((total, item) => {
-        const itemDate = new Date(item.date);
-        if (itemDate.toDateString() === currentDate.toDateString()) {
-            return total + parseFloat(item.price.toFixed(2));
-        }
-        return total;
-    }, 0);
+    const spentToday = filterSpent(spent, currentDate, "today");
+    const spentLastWeek = filterSpent(spent, currentDate, "lastWeek");
 
-    const totalMonthlySpending = spent.reduce((total, item) => {
-        return total + parseFloat(item.price.toFixed(2));
-    }, 0);
-
-    const spentFilter = spent.filter((spent) => {
-        return spent.date.toDateString() === currentDate.toDateString();
-    });
-
-    spentFilter.sort((a, b) => {
-        return Number(b.date) - Number(a.date);
-    });
+    spentToday.sort((a: any, b: any) => Number(b.date) - Number(a.date));
+    spentLastWeek.sort((a: any, b: any) => Number(b.price) - Number(a.price));
 
     return (
         <div>
             <Header name={"Gastos"} />
-            <div className="px-5 pt-5">
-                <h2 className="text-xl font-bold">
+            <div className="px-5 pt-5 lg:px-16">
+                <h2 className="text-xl font-bold lg:text-2xl">
                     {session?.user
                         ? `Olá, ${session.user.name?.split(" ")[0]}!`
                         : `Olá, Faça seu login!`}
                 </h2>
-                <p className="text-lg">Qual foi seu gasto hoje?</p>
+                <p className="text-lg lg:text-xl">Qual foi seu gasto hoje?</p>
                 <p className="capitalize text-sm">
                     {format(new Date(), "EEEE',' d 'de' MMMM", {
                         locale: ptBR,
@@ -68,22 +48,39 @@ export default async function Home() {
                         Total de gastos deste mês
                     </p>
                     <h1 className="text-5xl font-semibold text-destructive mt-3">
-                        - R$ {totalMonthlySpending.toFixed(2)}
+                        - R$ {totalMonth.toFixed(2)}
                     </h1>
                 </div>
             </div>
-
-            <div className="p-5 w-full flex items-center justify-between mt-5">
-                <h2 className="text-lg font-semibold">Hoje</h2>
-                <h2 className="text-lg font-semibold text-destructive">
-                    - R$ {totalTodaySpending.toFixed(2)}
-                </h2>
-            </div>
-
-            <div className="flex flex-col gap-3 p-5">
-                {spentFilter.map((spent) => (
-                    <SpentItem key={spent.id} spent={spent} />
-                ))}
+            <div className="flex justify-between lg:px-16">
+                <div className="hidden lg:block w-full">
+                    <div className="p-5 w-full flex items-center justify-between mt-5">
+                        <h2 className="text-lg font-semibold">Ultima Semana</h2>
+                        <h2 className="text-lg font-semibold text-destructive">
+                            - R$ {totalWeek.toFixed(2)}
+                        </h2>
+                    </div>
+                    <div className="flex flex-col gap-3 p-5 ">
+                        {spentLastWeek.map((spent: Spent) => (
+                            <SpentItem key={spent.id} spent={spent} />
+                        ))}
+                    </div>
+                </div>
+                {totalToday >= 1 && (
+                    <div className="w-full">
+                        <div className="p-5 w-full flex items-center justify-between mt-5">
+                            <h2 className="text-lg font-semibold">Hoje</h2>
+                            <h2 className="text-lg font-semibold text-destructive">
+                                - R$ {totalToday.toFixed(2)}
+                            </h2>
+                        </div>
+                        <div className="flex flex-col gap-3 p-5 ">
+                            {spentToday.map((spent: Spent) => (
+                                <SpentItem key={spent.id} spent={spent} />
+                            ))}
+                        </div>
+                    </div>
+                )}
             </div>
         </div>
     );
