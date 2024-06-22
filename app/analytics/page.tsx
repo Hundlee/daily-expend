88ba8f663563f972dab488844e92fcac5d55cc0c
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-06-22 20:39:42 -0300

    refactored code using functions like fetchSpentData

--- app/analytics/page.tsx
+++ app/analytics/page.tsx
@@ -6,26 +6,13 @@ import { endOfMonth, startOfMonth } from "date-fns";
 import { db } from "../_lib/prisma";
 import LastWeekSpent from "./_components/lastWeekSpent";
 import LastMonthSpent from "./_components/lastMonthSpent";
+import { fetchSpentData } from "../_helpers/fetchSpentData";
 
 const Analytics = async () => {
     const session = await getServerSession(authOptions);
-
     const currentDate = new Date();
-    const firstDayOfMonth = startOfMonth(currentDate);
-    const lastDayOfMonth = endOfMonth(currentDate);
 
-    const spent = session?.user
-        ? await db.spent.findMany({
-              where: {
-                  userId: (session.user as any).id,
-                  date: {
-                      gte: firstDayOfMonth,
-                      lte: lastDayOfMonth,
-                  },
-              },
-          })
-        : [];
-
+    const spent = await fetchSpentData(session, currentDate);
     return (
         <div>
             <Header name={"Análise"} />
